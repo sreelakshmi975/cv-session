e90483eb2d621f09747bd4d55656fb020ced5bec
sreelakshmi975 <87013908+sreelakshmi975@users.noreply.github.com>
2021-07-11 02:33:10 +0530

    Update question1.py

--- question1.py
+++ question1.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 
 img=cv2.imread('einstein.jpg',1)
 blur_gaus1 = cv2.GaussianBlur(img,(5,5),0)
-blur_gaus2 = cv2.GaussianBlur(img,(9,9),0)
+blur_gaus2 = cv2.GaussianBlur(img,(9,9),70)
 diff=blur_gaus1-blur_gaus2
 plt.figure(),plt.title("Final image"),plt.axis("off"),plt.imshow(diff)
 plt.show()
